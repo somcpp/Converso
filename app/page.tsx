ae9c2155b0072dedec3db036acd2a86572b08579
somcpp <saubhagya.sharma3.14@gmail.com>
2025-08-04 21:58:21 +0530

    intial setup and routing

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline">Hello World!</h1>
+        <Button>Button</Button>
+    </div>
+  )
+}
+
+export default Page
